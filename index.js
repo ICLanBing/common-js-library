0747a6e820aed721c56ab028067d3244c6bbd407
ICLanBing <>
2024-04-12 15:16:42 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+import lib_base64 from "./src/base64";
+import lib_clone_object from "./src/clone-object";
+import lib_color from "./src/color";
+import lib_cookie from "./src/cookie";
+import lib_crc32 from "./src/crc32";
+import lib_create_style from "./src/create-style";
+import lib_detect_flash from "./src/detect-flash";
+import lib_encode_html from "./src/encode-html";
+import lib_flatten_array from "./src/flatten-array";
+import lib_heap_size from "./src/heap-size";
+import lib_legacy_klass from "./src/legacy-klass";
+import lib_levenshtein from "./src/levenshtein";
+import lib_md5 from "./src/md5";
+import lib_poisson_distribution from "./src/poisson-distribution";
+import lib_sha1 from "./src/sha1";
+import lib_sha256 from "./src/sha256";
+import lib_show_boxes from "./src/show-boxes";
+import lib_shuffle from "./src/shuffle";
+import lib_sudoku_solver from "./src/sudoku-solver";
+import lib_url from "./src/url";
+import lib_utf8 from "./src/utf8";
+import lib_y_combinator from "./src/y-combinator";
+
+export { 
+    lib_base64, 
+    lib_clone_object, 
+    lib_color, 
+    lib_cookie, 
+    lib_crc32, 
+    lib_create_style, 
+    lib_detect_flash, 
+    lib_encode_html, 
+    lib_flatten_array, 
+    lib_heap_size,
+    lib_legacy_klass,
+    lib_levenshtein,
+    lib_md5,
+    lib_poisson_distribution,
+    lib_sha1,
+    lib_sha256,
+    lib_show_boxes,
+    lib_shuffle,
+    lib_sudoku_solver,
+    lib_url,
+    lib_utf8,
+    lib_y_combinator
+};
